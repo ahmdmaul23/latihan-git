4f1acc7c765c37dc4935068dc1b018367514dcdf
ahmdmaul23 <salimmaula23@gmail.com>
2022-04-04 19:33:38 +0700

    menambahkan biodata.php

---
+++ application/Controllers/biodata.php
@@ -0,0 +1,14 @@
+<?php
+
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ahmad Salim Maula
+        Saya tinggal di Cengkareng, Jakarta Barat.
+        Saya menyukai teknologi karena memudahkan suatu pekerjaan,
+        karena itu saya belajar mengenai Teknologi Informasi";
+    }
+}
+?>
