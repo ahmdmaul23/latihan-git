c35097260313641b1e8d78c4ea2f510bd54978a6
ahmdmaul23 <salimmaula23@gmail.com>
2022-04-05 10:18:56 +0700

    update file v_header.php

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -15,7 +15,7 @@
 			<nav>
 				<ul>
 					<li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
-					<li><a href="<?php echo base_url().'web/about' ?>">About</a></li>
+					<li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
 				</ul>
 			</nav>
 	<div class="clear"></div>
